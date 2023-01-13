cacd083fe9261266771240640aab2fe213ae34db
li_zhi_feng <lizhifeng@zuzuche.com>
2023-01-13 11:36:32 +0800

    feature:直销和europcar对接

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  { text: "演示", icon: "discover", link: "/concurrent/" },
+  { text: "文档模式", icon: "discover", link: "/concurrent/" },
   {
     text: "博客指南",
     icon: "note",

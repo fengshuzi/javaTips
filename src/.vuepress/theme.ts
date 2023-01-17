c0699cec2039a95018709ee5bca0bff0e4719cde
li_zhi_feng <lizhifeng@zuzuche.com>
2023-01-17 10:55:03 +0800

    读书计划

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -58,33 +58,6 @@ export default hopeTheme({
 
   plugins: {
     blog: true,
-
-    // If you don’t need comment feature, you can remove following option
-    // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
-    // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
-    comment: {
-      /**
-       * Using Giscus
-       */
-      provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
-      category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
-
-      /**
-       * Using Twikoo
-       */
-      // provider: "Twikoo",
-      // envId: "https://twikoo.ccknbc.vercel.app",
-
-      /**
-       * Using Waline
-       */
-      // provider: "Waline",
-      // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
-    },
-
     // Disable features you don’t want here
     mdEnhance: {
       align: true,

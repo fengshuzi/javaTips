644581e33e86625ee4c821648ba23c9b4ba42b12
li_zhi_feng <lizhifeng@zuzuche.com>
2023-01-17 10:05:54 +0800

    读书计划

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -3,9 +3,5 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/",
   { text: "文档模式", icon: "discover", link: "/concurrent/" },
-  {
-    text: "博客指南",
-    icon: "note",
-    link: "https://theme-hope.vuejs.press/zh/guide/",
-  },
+
 ]);

c4c855dbc30b763d4c807f47c76461bb49df9432
li_zhi_feng <lizhifeng@zuzuche.com>
2023-01-18 14:19:15 +0800

    读书计划

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  { text: "文档模式", icon: "discover", link: "/concurrent/" },
+  { text: "并发编程", icon: "discover", link: "/concurrent/" },
   { text: "读书计划", icon: "discover", link: "/reading/" },
 
 ]);
